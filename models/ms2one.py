5462e0b4b018a5845667e005e231c46cd820b09e
M. Fachry Dwi Handoko <87126965+Delos-343@users.noreply.github.com>
2025-01-22 23:22:58 +0700

    Workable version for ms2one.py

--- models/ms2one.py
+++ models/ms2one.py
@@ -16,6 +16,19 @@ def build_ms2one(config):
 
 
 class Naive(nn.Module):
+    """_summary_
+    Naive Class:
+    Purpose: This class implements a basic convolutional layer with 1x1 kernel size.
+    It is designed to operate on multi-scale feature maps.
+    Attributes:
+    inc: Number of input channels.
+    outc: Number of output channels.
+    kernel_size: Size of the convolutional kernel (default is 1).
+    Methods:
+    __init__: Initializes the convolutional layer.
+    forward: Defines the forward pass of the layer, which involves interpolating and 
+    concatenating feature maps before passing them through the convolutional layer.
+    """
     def __init__(self, inc, outc, kernel_size=1):
         super().__init__()
         self.layer = nn.Conv2d(inc, outc, kernel_size=1)
@@ -28,22 +41,64 @@ class Naive(nn.Module):
 
 
 class DilateNaive(nn.Module):
+    """_summary_
+    DilateNaive Class:
+    Purpose: This class implements a network with dilated convolutions, allowing for an increased receptive field without downsampling the input.
+    Attributes:
+    inc: Number of input channels for each scale
+    outc: Number of output channels.
+    num_scales: Number of scales (default is 4).
+    dilations: A tuple specifying the dilation rates for each scale.
+    merge: Whether to merge the outputs from different scales (default is True).
+    fpn: Whether to use feature pyramid network-like merging (default is False).
+    target_shape: If specified, the target shape for the output.
+    one_layer_before: Whether to apply one additional layer before dilated convolutions (default is False).
+    Methods:
+    __init__: Initializes the DilateNaive module.
+    forward: Defines the forward pass, which involves applying dilated convolutions on each scale and optionally merging the results.
+    """
     def __init__(self, inc, outc, num_scales=4,
                  dilations=(1, 2, 5, 9),
                  merge=True, fpn=False,
                  target_shape=None,
                  one_layer_before=False):
+        
         super().__init__()
+
+        # [1, 2, 5 , 9]
         self.dilations = dilations
         self.num_scales = num_scales
+
         if not isinstance(inc, (tuple, list)):
             inc = [inc for _ in range(num_scales)]
+
         self.inc = inc
         self.outc = outc
         self.merge = merge
         self.fpn = fpn
         self.target_shape = target_shape
         self.layers = nn.ModuleList()
+
+        """
+        # Implementation of a neural network module using dilated convolutions
+        # to capture spatial features at different scales. The model consists
+        # of multiple scales, each containing dilated convolution layers with
+        # varying dilation rates. The `num_scales` parameter determines the
+        # number of scales. Each scale may have multiple dilated convolution
+        # layers (`j` iterations), and the dilation rates decrease with depth.
+        # The chosen dilation values (1, 2, 5, 9) increase gradually, enhancing
+        # the model's ability to recognize features at different spatial scales.
+        # If `one_layer_before` is True, an additional convolution layer with
+        # batch normalization and ReLU activation is applied before dilated
+        # convolutions to reduce input size. The model can merge outputs from
+        # different scales (`merge=True`). If `fpn` is also True, feature pyramid
+        # network-like merging is employed. The `target_shape` parameter can be
+        # used to specify the desired output shape. The final layer, if merging is
+        # enabled, combines the scaled outputs. Overall, this architecture enables
+        # the network to efficiently capture spatial information across multiple
+        # scales without downsampling the input.
+        """
+
         for i in range(num_scales):
             layers = []
             if one_layer_before:
@@ -52,6 +107,7 @@ class DilateNaive(nn.Module):
                     nn.BatchNorm2d(outc),
                     nn.ReLU(True)
                 ])
+
             for j in range(len(dilations[:-i])):
                 d = dilations[j]
                 layers.append(nn.Sequential(
@@ -63,7 +119,9 @@ class DilateNaive(nn.Module):
                               bias=False),
                     nn.BatchNorm2d(outc),
                     nn.ReLU(True)))
+                
             self.layers.append(nn.Sequential(*layers))
+
         if self.merge:
             self.final_layer = nn.Sequential(
                 nn.Conv2d(outc, outc, 3, 1, padding=1, bias=False),
