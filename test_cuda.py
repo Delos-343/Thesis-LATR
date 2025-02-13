84e193fa2b7c2de16c9ce9d7f0754b63c80cc576
M. Fachry Dwi Handoko <87126965+Delos-343@users.noreply.github.com>
2025-02-14 01:55:15 +0700

    Add files via upload

--- test_cuda.py
+++ test_cuda.py
@@ -1,6 +1,38 @@
 import torch
 import PIL
 
-print(torch.cuda.is_available())
+print("\n")
+
+print(torch.cuda.is_available())  # Should return True
+
+print("\n")
+
+print(torch.cuda.current_device())  # Should return the current device index
+
+print("\n")
+
+print(torch.cuda.get_device_name(0))  # Should return the name of your GPU
+
+print("\n")
+
+print(torch.cuda.device_count())
+
+print("\n")
+
+print(torch.cuda.get_arch_list())  # Should return the sm_x versions of your device architecture
+
+print("\n")
+
+print(torch.version.cuda)  # Should print '10.1'
+
+print("\n")
+
+print(torch.backends.cudnn.version())
+
+print("\n")
+
+print(torch.backends.cudnn.is_available())  # Should print True
+
+print("\n")
 
 print(PIL.__version__)
